9324c32301785dc4d3db3868248d46154f8adf18
Mohammed <2175006m@student.gla.ac.uk>
2018-02-09 16:13:38 +0000

    End of Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
