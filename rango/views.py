e9685359004bba7879a4249cac584eb85b78dc3a
Mohammed <2175006m@student.gla.ac.uk>
2018-02-09 18:20:30 +0000

    End of Chapter 10 (fix)

--- rango/views.py
+++ rango/views.py
@@ -162,20 +162,6 @@ def user_logout(request):
 def restricted(request):
     return render(request, 'rango/restricted.html', {})
 
-def visitor_cookie_handler(request, response):
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
-
-    if (datetime.now() - last_visit_time).days > 0:
-        visits = visits + 1
-        response.set_cookie('last_visit', str(datetime.now()))
-
-    else:
-        visits = 1
-        response.set_cookie('last_visit', last_visit_cookie)
-
-    response.set_cookie('visits', visits)
 
 def get_server_side_cookie(request, cookie, default_val=None):
     val = request.session.get(cookie)
